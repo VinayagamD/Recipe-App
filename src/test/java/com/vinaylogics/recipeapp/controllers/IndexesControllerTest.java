ac88620700891f26b5f7b7614706e6e8d0c4b539
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-06-22 23:59:54 +0530

    AgrumentCaptor Test

--- src/test/java/com/vinaylogics/recipeapp/controllers/IndexesControllerTest.java
+++ src/test/java/com/vinaylogics/recipeapp/controllers/IndexesControllerTest.java
@@ -1,15 +1,19 @@
 package com.vinaylogics.recipeapp.controllers;
 
+import com.vinaylogics.recipeapp.domain.Recipe;
 import com.vinaylogics.recipeapp.services.RecipeService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.Mockito.*;
 
 /**
@@ -40,9 +44,24 @@ public class IndexesControllerTest {
      */
     @Test
     public void getIndexPage() {
+//        given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(1l);
+        recipes.add(recipe);
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //When
         String viewName = indexesController.getIndexPage(model);
+
+        //Then
         assertEquals("index",viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
